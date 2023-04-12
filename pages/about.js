01cca732147c3c19b6c8757e8ba080612fa16077
i-fateh <fateh180581@gmail.com>
2023-04-12 15:21:26 +0200

    second commit

--- pages/about.js
+++ pages/about.js
@@ -9,12 +9,12 @@ function About() {
         <Navigation />
         <div className={styles.container}>
         <div>
-          <h1 className={styles.h1}>De l'idée à la solution</h1>
+          <h1 className={styles.h1}>De l&apos;idée à la solution</h1>
           <p className={styles.paragraphe}>
-          Bienvenue chez Fake Project Agency, l'agence de création de site internet et d'application web qui donne vie à vos idées les plus audacieuses! 
+          Bienvenue chez Fake Project Agency, l&apos;agence de création de site internet et d&apos;application web qui donne vie à vos idées les plus audacieuses! 
           <br/>
           <br/>
-          Nous sommes des passionnés du web, déterminés à offrir à nos clients une expérience numérique unique et mémorable. Que vous cherchiez à lancer un site de commerce électronique, une application mobile ou un site vitrine pour votre entreprise, l'agence Fake Project Agency est là pour vous accompagner tout au long de votre projet.
+          Nous sommes des passionnés du web, déterminés à offrir à nos clients une expérience numérique unique et mémorable. Que vous cherchiez à lancer un site de commerce électronique, une application mobile ou un site vitrine pour votre entreprise, l&apos;agence Fake Project Agency est là pour vous accompagner tout au long de votre projet.
           <br/>
           </p>
         </div>
@@ -111,8 +111,8 @@ function About() {
 
         <p className={styles.paragraphe}>
         <br/>
-        L'agence Fake Project Agency se distingue par son expertise technique, sa créativité et son engagement à fournir des solutions sur mesure adaptées à chaque projet. 
-        Nous offrons également des services de référencement et d'hébergement pour vous assurer que votre site ou application fonctionne à la perfection et attire l'attention qu'il mérite.
+        L&apos;agence Fake Project Agency se distingue par son expertise technique, sa créativité et son engagement à fournir des solutions sur mesure adaptées à chaque projet. 
+        Nous offrons également des services de référencement et d&apos;hébergement pour vous assurer que votre site ou application fonctionne à la perfection et attire l&apos;attention qu&apos;il mérite.
         </p>
 
         <div className={styles.banner}>
